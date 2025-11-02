2169ce8aa9c84ddc8ae997be61b3ca63439ce309
SIGMAxDEV <ayush.riyal@outlook.com>
2025-11-02 20:34:51 +0530

    Update bot.py

--- api/bot.py
+++ api/bot.py
@@ -4,7 +4,7 @@ from flask import Flask, request
 
 app = Flask(name)
 
-TOKEN = os.environ.get("BOT_TOKEN")
+TOKEN = os.environ.get("7200494679:AAH-W84VIBRVKnoZoSNHPIXkgNawCsjubxo")
 URL = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
 
 @app.route("/api/bot", methods=["POST", "GET"])
