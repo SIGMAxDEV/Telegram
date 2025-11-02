b9bc89e5c25a7b9c365c52a26077e026459101f7
SIGMAxDEV <ayush.riyal@outlook.com>
2025-11-02 21:02:56 +0530

    Update index.py

--- index.py
+++ index.py
@@ -11,12 +11,12 @@ if not TOKEN:
 URL = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
 
 
-@app.route("/api", methods=["GET"])
+@app.route("/api/bot", methods=["GET"])
 def home():
     return jsonify({"status": "Bot running successfully 🚀"}), 200
 
 
-@app.route("/api", methods=["POST"])
+@app.route("/api/bot", methods=["POST"])
 def webhook():
     data = request.get_json()
     if "message" in data:
@@ -24,7 +24,7 @@ def webhook():
         text = data["message"].get("text", "")
 
         if text == "/start":
-            reply = "Hello 🔑 This bot is running on Vercel 🚀"
+            reply = "Hello Ayu! This bot is running on Vercel 🚀"
         else:
             reply = "I’m alive on Vercel ⚡"
 
